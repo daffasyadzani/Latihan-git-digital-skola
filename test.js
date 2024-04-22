c577834d2392f8208e4d8777dd45f81c2ac5e335
Daffa Pamungkas Syadzani <52237620+daffasyadzani@users.noreply.github.com>
2024-04-22 21:50:55 +0700

    test.js

---
+++ test.js
@@ -0,0 +1,35 @@
+function kalkul(a, b, op) {
+
+ switch (op) {
+
+ 
+
+ case '+':
+
+ return a + b
+
+ break;
+
+ case '-':
+
+ return a - b
+
+ break;
+
+ case '*':
+
+ return a * b
+
+ break;
+
+ case '/':
+
+ return a / b
+
+ break;
+
+    }
+
+  }
+
+ console.log(kalkul(5, 5, '/')); 
